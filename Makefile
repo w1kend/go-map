29c108dd7b36d5eebf5fc1bb028cc229002dad0b
Sergei Makarov <wikendp@gmail.com>
2022-11-21 00:39:44 +0400

    map overflow benchmark added

--- Makefile
+++ Makefile
@@ -4,5 +4,8 @@ test:
 bench:
 	go test . -run=^$$ -bench . -benchmem
 
+bench-overflow:
+	go test . -run=^$$ -bench ^BenchmarkPutWithOverflow$$ -benchmem
+
 fuzz:
 	go test -run=^$$ -fuzz FuzzMap
--- map_benchmark_test.go
+++ map_benchmark_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-var sizes = []int{128, 1024, 4096, 8192}
+var sizes = []int{128, 1024, 8192}
 
 func BenchmarkGet(b *testing.B) {
 	for _, n := range sizes {
@@ -14,7 +14,7 @@ func BenchmarkGet(b *testing.B) {
 			mm.Put(fmt.Sprintf("key__%d", i), int64(i)*2)
 		}
 
-		b.Run(fmt.Sprintf("my-map_%d", n), func(b *testing.B) {
+		b.Run(fmt.Sprintf("generic-map_%d", n), func(b *testing.B) {
 			var got int64
 			j := 0
 			for i := 0; i < b.N; i++ {
@@ -52,7 +52,7 @@ func BenchmarkGet(b *testing.B) {
 func BenchmarkPut(b *testing.B) {
 	for _, n := range sizes {
 		mm := New[int64](n)
-		b.Run(fmt.Sprintf("my-map_%d", n), func(b *testing.B) {
+		b.Run(fmt.Sprintf("generic-map_%d", n), func(b *testing.B) {
 			j := 0
 			for i := 0; i < b.N; i++ {
 				if j > n {
@@ -67,6 +67,39 @@ func BenchmarkPut(b *testing.B) {
 	for _, n := range sizes {
 		stdm := make(map[string]int64, n)
 		b.Run(fmt.Sprintf("std-map_%d", n), func(b *testing.B) {
+			j := 0
+			for i := 0; i < b.N; i++ {
+				if j > n {
+					j = 0
+				}
+				stdm[fmt.Sprintf("key__%d", j)] = int64(j)
+				j++
+			}
+		})
+	}
+}
+
+func BenchmarkPutWithOverflow(b *testing.B) {
+	startSize := 1_000
+	targetSize := []int{10_000, 100_000, 1_000_000}
+
+	for _, n := range targetSize {
+		mm := New[int64](startSize)
+		b.Run(fmt.Sprintf("generic-map-with-overflow__%d", n), func(b *testing.B) {
+			j := 0
+			for i := 0; i < b.N; i++ {
+				if j > n {
+					j = 0
+				}
+				mm.Put(fmt.Sprintf("key__%d", j), int64(j))
+				j++
+			}
+		})
+	}
+
+	for _, n := range targetSize {
+		stdm := make(map[string]int64, startSize)
+		b.Run(fmt.Sprintf("std-map-with-evacuation__%d", n), func(b *testing.B) {
 			j := 0
 			for i := 0; i < b.N; i++ {
 				if j > n {
