58d7c025500d82b1164968ececb853f3d6e304e3
Sergei Makarov <wikendp@gmail.com>
2023-01-26 09:55:24 +0400

    const check description

--- const_check.go
+++ const_check.go
@@ -1,7 +1,12 @@
 package gomap
 
+// this is a compile time check for const values. instead a check below
+// wich happens every time during an evacuate() func under the hood.
+//
+//	if evacuatedX+1 != evacuatedY || evacuatedX^1 != evacuatedY {
+//		throw("bad evacuatedN")
+//	}
 func _() {
-	// compile time check for changed const values
 	var x [1]struct{}
 	_ = x[evacuatedFirst-2]
 	_ = x[evacuatedSecond-3]
